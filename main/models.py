4da3b7f3d60d15b55bd490dc9fee5d6c10834c88
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2019-02-21 16:04:33 +0530

    Model structure done

---
+++ main/models.py
@@ -0,0 +1,16 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete = models.SET_NULL, null=True)
+    name = models.CharField(max_length = 100)
+    phone = models.BigIntegerField()
+    emergency_phone = models.BigIntegerField()
+    email = models.EmailField(unique = True)
+    lat = models.DecimalField(max_digits = 10, decimal_places=6)
+    long = models.DecimalField(max_digits = 10, decimal_places=6)
+    is_safe = models.BooleanField(default=True, blank=True)
+
+    def __str__(self):
+        return self.name
---
+++ payments/models.py
@@ -0,0 +1,23 @@
+
+from django.db import models
+from main.models import UserProfile
+from django.utils import timezone
+
+
+class Event(models.Model):
+    admin = models.OneToOneField('UserProfile', on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField(default='NA')
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    transfer_to = models.ForeignKey('UserProfile', on_delete=models.SET_NULL, related_name = 'transferred_to', null=True)
+    transfer_from = models.ForeignKey('UserProfile', on_delete = models.SET_NULL, related_name='transferred_from', null=True)
+    amount = models.PositiveIntegerField(default=0)
+    payment_id = models.CharField(null=True, max_length=30, unique=True)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "{} from {} to {}".format(self.amount, self.transaction_from.name, self.transfer_to.name)
