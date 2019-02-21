d7b2327608465b8f456b0fa90c041f4efa297f01
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2019-02-21 16:11:39 +0530

    Migrations and Some minor model issue sort

--- payments/admin.py
+++ payments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Event, Transaction
+from .models import Event, Transaction
 
 admin.site.register(Event)
 admin.site.register(Transaction)
--- payments/models.py
+++ payments/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 
 class Event(models.Model):
-    admin = models.OneToOneField('UserProfile', on_delete=models.CASCADE, null=True)
+    admin = models.OneToOneField('main.UserProfile', on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=50)
     description = models.TextField(default='NA')
 
@@ -13,8 +13,8 @@ class Event(models.Model):
         return self.name
 
 class Transaction(models.Model):
-    transfer_to = models.ForeignKey('UserProfile', on_delete=models.SET_NULL, related_name = 'transferred_to', null=True)
-    transfer_from = models.ForeignKey('UserProfile', on_delete = models.SET_NULL, related_name='transferred_from', null=True)
+    transfer_to = models.ForeignKey('main.UserProfile', on_delete=models.SET_NULL, related_name = 'transferred_to', null=True)
+    transfer_from = models.ForeignKey('main.UserProfile', on_delete = models.SET_NULL, related_name='transferred_from', null=True)
     amount = models.PositiveIntegerField(default=0)
     payment_id = models.CharField(null=True, max_length=30, unique=True)
     timestamp = models.DateTimeField(default=timezone.now)
