404141ba38aa69b6217f2415a14525df14284212
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2019-02-24 12:20:41 +0530

    Static and Media URLS

--- sih/settings.py
+++ sih/settings.py
@@ -133,4 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/backend_static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/backend_media/'
