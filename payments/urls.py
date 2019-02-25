b104c87265e72a011450239642eafcf7a2e6f1ba
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2019-02-25 18:23:24 +0530

    Get details of a particular events

--- payments/urls.py
+++ payments/urls.py
@@ -3,5 +3,6 @@ from payments import views
 
 urlpatterns = [
 	url(r'^get_active_events/?',views.get_active_events,name='get_active_events'),
+	url(r'^get_event_details/(?P<event_id>\d+)/?$', views.get_event_details, name='get_event_details'),
 
 ]
--- payments/views.py
+++ payments/views.py
@@ -29,3 +29,22 @@ def get_active_events(request):
 
     elif request.method == 'POST':
         return JsonResponse({"message":"A <GET> Request only endpoint for getting details of active events."})
+
+def get_event_details(request, event_id):
+    if request.method == 'GET':
+
+        try:
+            user_id = str(request.META['HTTP_X_USER_ID'])
+        except KeyError:
+            return JsonResponse({"message":"Header missing: X-USER-ID", "status":2})
+
+        try:
+            user_profile = UserProfile.objects.get(uuid=user_id)
+        except Exception:
+            return JsonResponse({"message":"The given UserId doesnt correspond to any user."})
+
+        event_details =  Event.objects.get(id = event_id).values('id', 'name', 'description', 'fund_goal')
+        return JsonResponse({"event":list(event_details)})
+
+    elif request.method == 'POST':
+        return JsonResponse({"message":"A <GET> only Request endpoint for getting details of active events."})
