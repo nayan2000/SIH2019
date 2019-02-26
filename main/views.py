242cccd09642716eff2a7269e85ca9c5d4740d5a
Chinmay Hebbar <4chinmai@gmail.com>
2019-02-26 22:18:42 +0530

    add decorator to convert to jsonp

--- main/views.py
+++ main/views.py
@@ -28,7 +28,31 @@ from sih.keyconfig import SENDGRID_API_KEY, FIREBASE_API_KEY, FCM_URL
 chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
 url = 'http://alertify.org'
 
+
+def json_response(func):
+    """
+    A decorator thats takes a view response and turns it
+    into jsonp. If a callback is added through GET or POST
+    the response is JSONP.
+    """
+    def decorator(request, *args, **kwargs):
+        objects = func(request, *args, **kwargs)
+        if isinstance(objects, HttpResponse):
+            return objects
+        try:
+            data = objects
+            if 'callback' in request.REQUEST:
+                # a jsonp response!
+                data = '%s(%s);' % (request.REQUEST['callback'], data)
+                return HttpResponse(data, "text/javascript")
+        except:
+             data = str(objects)
+        return HttpResponse(data, "application/json")
+    return decorator
+
+
 @csrf_exempt
+@json_response
 def get_location(request):
     if request.method == 'GET':
         user_profiles = UserProfile.objects.all().exclude(lat=0, long=0).values('lat','long')
