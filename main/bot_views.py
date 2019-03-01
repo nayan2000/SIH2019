bd0d882c82595ece0dfe3488612210cd069433ed
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2019-03-01 13:54:08 +0530

    Some prebuilt bot responses

--- main/bot_views.py
+++ main/bot_views.py
@@ -48,10 +48,28 @@ def command_response(request):
         except Exception:
             return JsonResponse({"message":"Invalid Command. Use !commands to see the list of commands."})
           
-        response = 'Command to see ' + str(command.short_description)
+        response = 'Command to see ' + str(command.response)
         #Now decide what response to be sent for the command and set it equal to response
         return JsonResponse({"message":response})
 
     else:
         return JsonResponse({"message":"Requests other than POST are not Supported"})
+
+
+'''
+!commands would give:
+Use !food to get latest Food Drop Locations.
+Use !safe to get the safe locations nearby you.
+Use !contact to get Emergency Contacts.
+Use !donate to Donate for the people in trouble.
+Use !intro to get to know more about the bot.
+Use !commands to get the list of commands.
+
+!donate would give:
+To donate for the people in trouble, go to the Donate section of the app and
+select a suitable amount and go ahead and pay. You're done.
+
+!intro would give:
+Hey! Myself AlertBot. I am here for your service. I am a part of the Rescue Operations Team.
+'''
 
